d89866b1751b102bb3a66744ffd8fb9bba9b83ae
NgocChau0209 <ngocchautrantp@gmail.com>
2021-07-16 11:35:24 +0700

    init project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css';
+import { Button } from '../components/Button';
+export default function Home() {
+  return (
+    <>
+      <Button/>
+    </>
+  )
+}
